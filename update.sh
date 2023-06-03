fb682ffff3e41bd592b84ef54f84915c4ecd400a
Dubhar <faebhir@gmail.com>
2023-06-03 22:18:47 +0200

    added styles

--- update.sh
+++ update.sh
@@ -2,25 +2,41 @@
 
 main() {
   echo "$(date +"%Y-%m-%d %H:%MZ") - updating website"
-  local reponame
-  reponame=$(basename "${REPO}")
+
   local dirname
-  dirname=${reponame%.*}
+  dirname=$(repo_uri_to_dir "${REPO}")
 
   update_git_files
   process_asciidoc
   publish
 }
 
-update_git_files() {
-  if [ -d "${dirname}" ]; then
+repo_uri_to_dir() {
+  local reponame
+  reponame=$(basename "${1}")
+  local dirname
+  echo "${reponame%.*}"
+}
+
+clone_or_pull() {
+  local repouri
+  repouri="${1}"
+  local repodir
+  repodir=$(repo_uri_to_dir "${repouri}")
+  if [ -d "${repodir}" ]; then
     (
-      cd "${dirname}" || exit
+      cd "${repodir}" || exit
       git -c core.sshCommand="ssh -i /ssh_key -o StrictHostKeyChecking=no" pull
     )
   else
-    git -c core.sshCommand="ssh -i /ssh_key -o StrictHostKeyChecking=no" clone "${REPO}"
+    git -c core.sshCommand="ssh -i /ssh_key -o StrictHostKeyChecking=no" clone "${repouri}"
   fi
+}
+
+update_git_files() {
+  clone_or_pull "${REPO}"
+  clone_or_pull "https://github.com/darshandsoni/asciidoctor-skins.git"
+  cp "asciidoctor-skins/css/*.css" "${dirname}/."
 }
 
 process_asciidoc() {
